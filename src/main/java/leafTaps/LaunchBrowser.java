145d0317a628db31e721dabab4a15b54682d4b5b
koushik <koushik@koushik-PC>
2018-02-19 21:59:11 +0530

    Launch browser

--- src/main/java/leafTaps/LaunchBrowser.java
+++ src/main/java/leafTaps/LaunchBrowser.java
@@ -7,31 +7,15 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 public class LaunchBrowser {
 
 	public static void main(String[] args) {
-		try {
+		
 			ChromeDriver driver = new ChromeDriver();
 			driver.get("https://google.com");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
-		try {
 			FirefoxDriver ff = new FirefoxDriver();
 			ff.get("https://google.com");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
-		try {
 			InternetExplorerDriver ie = new InternetExplorerDriver();
 			ie.get("https://google.com");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-
 	}
 
 }
