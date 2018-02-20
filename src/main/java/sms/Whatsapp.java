aaa0e4e2341723fb9fef9e72b07027e9497a8aa4
koushik <koushik@koushik-PC>
2018-02-20 21:44:47 +0530

    WhatsApp working - uploading file

--- src/main/java/sms/Whatsapp.java
+++ src/main/java/sms/Whatsapp.java
@@ -3,6 +3,7 @@ package sms;
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -14,8 +15,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class Whatsapp {
-	String groupName = "Hari Iyer";
-	String msg = "Chill da";
+	String groupName = "9962251412";
+	String msg = "test";
+	String path = "H:\\a.jpg";
 	@Test
 	public void watsap() {
 		ChromeDriver driver = new ChromeDriver();
@@ -24,18 +26,35 @@ public class Whatsapp {
 		driver.get("https://web.whatsapp.com/");
 		WebDriverWait wait = new WebDriverWait(driver, 30);
 		LocalFileDetector detect = new LocalFileDetector();
-		WebElement logo = driver.findElementByXPath("//span[@data-icon='logo']");
-		wait.until(ExpectedConditions.invisibilityOf(logo));
+		try {
+			WebElement logo = driver.findElementByXPath("//span[@data-icon='logo']");
+			wait.until(ExpectedConditions.invisibilityOf(logo));
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
 		JavascriptExecutor js = ((JavascriptExecutor)driver);
 		js.executeScript("return !!window.jQuery && window.jQuery.active == 0");
 		WebElement search = driver.findElementById("input-chatlist-search");
 		wait.until(ExpectedConditions.visibilityOf(search));
-		search.sendKeys(groupName, Keys.ENTER, msg, Keys.ENTER);
+		search.sendKeys(groupName);/*, Keys.ENTER, msg, Keys.ENTER);*/
+		WebElement searchResult = driver.findElementByXPath("(//span[@title='+91 99622 51412'])[1]");
+		searchResult.click();
+		driver.switchTo().activeElement().sendKeys(msg, Keys.ENTER);		
+		driver.findElementByXPath("//span[@data-icon='clip']").click();
 		WebElement uploadFile = driver.findElementByXPath("(//input[@type='file'])[1]");
-		String path = "E:\\a.jpg";
 		File f = detect.getLocalFile(path);
 		((RemoteWebElement)uploadFile).setFileDetector(detect);
 		uploadFile.sendKeys(f.getAbsolutePath());
+		wait.until(ExpectedConditions.
+				presenceOfElementLocated(By.xpath("//span[text()='Add a caption…']"))).sendKeys("test");
+		driver.findElementByXPath("//span[@data-icon='send-light']").click();
+		try {
+			WebElement uploadText = driver.
+					findElementByXPath("(//div[@class='pluggable-input-body copyable-text selectable-text'])[1]");
+			uploadText.sendKeys("test",Keys.ENTER);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 
 
