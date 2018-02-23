e4b88d3ffe7c8c53817d6a408c7662b3ebcfeb98
koushik <koushik@koushik-PC>
2018-02-23 21:47:19 +0530

    Eachchar - java

---
+++ src/main/java/tricksJavaSel/EachCharCount.java
@@ -0,0 +1,36 @@
+package tricksJavaSel;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class EachCharCount {
+
+	public static void main(String[] args) {
+	/*	String txt ="aabbccadf";
+		char[] ch = txt.toCharArray();		
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		for (char c : ch) {			
+			if(map.containsKey(c)){				
+				Integer count = map.get(c)+1;
+				map.put(c, count);				
+			}else{
+				map.put(c, 1);
+			}			
+		}		
+		System.out.println(map);
+*/
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		map.put('c', 1);
+		map.put('n', 1);
+		map.put('c', 2);
+		System.out.println(map);
+		for (Entry<Character, Integer> ch : map.entrySet()) {
+			System.out.println(ch.getKey()+"-->"+ch.getValue());
+		}
+		System.out.println(map.containsKey('c'));
+		System.out.println(map.containsValue(2));
+	}
+
+}
+
