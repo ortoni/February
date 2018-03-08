f69097f13dc0196ca1997fc10caa12d8a51e7130
dell <dell@dell-PC>
2018-03-08 12:47:52 +0530

    crciket

--- src/main/java/leafTaps/Cricket.java
+++ src/main/java/leafTaps/Cricket.java
@@ -1,11 +1,11 @@
 package leafTaps;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Cricket {
@@ -14,40 +14,7 @@ public class Cricket {
 		d.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		d.get("https://www.google.co.in");
 		d.findElementById("lst-ib").sendKeys("cricket", Keys.ENTER);
-		Thread.sleep(5000);
-		String cr = d.getPageSource();
-		System.out.println(cr);
-
-		String pattern = "\\w+";
-
-		// Compile the pattern
-		Pattern p = Pattern.compile(pattern);
-
-		// Get the matcher
-		Matcher match = p.matcher(cr);		
-
-		// if multiple matches
-		if (match.find()) {
-				System.out.println(match.group().contains("cricket"));
-			System.out.println(match.group());            
-		}
-		/*// Make a URL to the web page
-		URL url = new URL("http://stackoverflow.com/questions/6159118/using-java-to-pull-data-from-a-webpage"); 
-		// Get the input stream through URL Connection
-		URLConnection con = url.openConnection();
-		InputStream is =con.getInputStream(); 
-		// Once you have the Input Stream, it's just plain old Java IO stuff.
-		// For this case, since you are interested in getting plain-text web page
-		// I'll use a reader and output the text content to System.out.
-		// For binary content, it's better to directly read the bytes from stream and write
-		// to the target file.
-		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-		String line = null; 
-		// read each line and write to System.out
-		while ((line = br.readLine()) != null) {
-			System.out.println(line); }*/
-	}	
-
-
-
+		List<WebElement> cr = d.findElementsByXPath("//*[contains(text(),'cricket') or contains(text(),'Cricket')]");
+		System.out.println(cr.size());
+	}
 }
