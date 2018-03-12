562ee486732384bbb7cbc8fc6f2c5af03d9bcc20
dell <dell@dell-PC>
2018-03-13 01:16:39 +0530

    Cleartrip

---
+++ src/main/java/leafTaps/ClearTrip.java
@@ -0,0 +1,18 @@
+package leafTaps;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ClearTrip {
+	@Test
+	public void dev() throws InterruptedException {
+		//System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
+		driver.manage().window().maximize();
+		driver.get("https://www.cleartrip.com/");
+		driver.findElementByXPath("(//i[@class='icon ir datePicker'])[1]").click();
+	}
+}
