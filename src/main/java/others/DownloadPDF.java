e34ceff8fc02a79bd363dba55ad6d4246fe577dd
ortoni <koushik350@gmail.com>
2018-04-04 00:46:54 +0530

    Download pdf

--- src/main/java/others/DownloadPDF.java
+++ src/main/java/others/DownloadPDF.java
@@ -2,28 +2,38 @@ package others;
 
 import java.util.HashMap;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-public class DownloadFiles {
+public class DownloadPDF {
 	RemoteWebDriver driver;
 	ChromeOptions options;
 	String downloadFilepath = "K:\\";
 	HashMap<String, Object> prefs;
+	Actions builder;
 	@Test
-	public void downloadFiles()	{
+	public void pdf() {
 		options	 = new ChromeOptions();
 		prefs = new HashMap<String, Object>();	// To put preferences using "prefs" (KeyWord)
 		prefs.put("download.default_directory", downloadFilepath); //To set path
 		prefs.put("safebrowsing.enabled", "false"); // To disable security check (keep or Cancel)
-        options.addArguments("plugins.plugins_disabled", "Chrome PDF Viewer");
+		prefs.put("plugins.always_open_pdf_externally", true);
 		options.setExperimentalOption("prefs", prefs); // Adding preferences to ChromeOptions
 		options.addArguments("--disable-extensions"); //to disable window browser
 		driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
-		driver.get("https://www.win-rar.com/predownload.html");
-		driver.findElementByLinkText("Download WinRAR").click();
+		driver.get("http://enos.itcollege.ee/~jpoial/allalaadimised/reading/");
+		WebElement pdf = driver.findElementByLinkText("Advanced-java.pdf");
+		pdf.click();
 	}
+	public void openInNewTab(WebElement ele) {
+		builder = new Actions(driver);
+		builder.sendKeys(ele,Keys.CONTROL).click().perform();
+	}
+
 }
