4f40a8143207c7e6deaadfc5bfa46a789cf9e4b0
ortoni <koushik350@gmail.com>
2018-04-04 16:14:59 +0530

    download

--- src/main/java/others/DownloadFiles.java
+++ src/main/java/others/DownloadFiles.java
@@ -18,7 +18,7 @@ public class DownloadFiles {
 		prefs = new HashMap<String, Object>();	// To put preferences using "prefs" (KeyWord)
 		prefs.put("download.default_directory", downloadFilepath); //To set path
 		prefs.put("safebrowsing.enabled", "false"); // To disable security check (keep or Cancel)
-        options.addArguments("plugins.plugins_disabled", "Chrome PDF Viewer");
+        options.addArguments("plugins.plugins_disabled", "Chrome PDF Viewer"); //to download PDF
 		options.setExperimentalOption("prefs", prefs); // Adding preferences to ChromeOptions
 		options.addArguments("--disable-extensions"); //to disable window browser
 		driver = new ChromeDriver(options);
