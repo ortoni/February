14eaedb001035c473affecdb65c9f52a5f30b433
ortoni <koushik350@gmail.com>
2018-04-16 12:37:12 +0530

    auth

--- src/main/java/alertAuthenticate/NetGearAuthenticate.java
+++ src/main/java/alertAuthenticate/NetGearAuthenticate.java
@@ -1,17 +1,28 @@
 package alertAuthenticate;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
 
 public class NetGearAuthenticate {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+
 		InternetExplorerOptions op = new InternetExplorerOptions();
-		op.setCapability("", "");
-		op.requireWindowFocus();
-		
-		InternetExplorerDriver driver = new InternetExplorerDriver();
-		driver.get("http://madmin:Micr0.com@merlin.rheincs.net");		
-		//driver.get("http://admin:password@192.168.1.1");
+		//op.setUnhandledPromptBehaviour(true);
+		InternetExplorerDriver driver = new InternetExplorerDriver(op);
+		//	driver.get("http://madmin:Micr0.com@merlin.rheincs.net");		
+		driver.get("http://admin:password@192.168.1.1");
+
+
+		//driver.get("http://seleniumhq.org/");
+		File src = driver.getScreenshotAs(OutputType.FILE);
+		File des = new File("./snap/i.jpg");
+		FileUtils.copyFile(src, des);
+
+
 	}
 }
