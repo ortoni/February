a736e35860340721cbf8e27545e25c1c0907ab64
Koushik <koushik350@gmail.com>
2018-05-29 16:03:46 +0530

    bank

--- src/main/java/others/BankBazaar.java
+++ src/main/java/others/BankBazaar.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class BankUsingif {
+public class BankBazaar {
 
 	public static void main(String[] args) throws InterruptedException {
 		ChromeDriver driver = new ChromeDriver();
